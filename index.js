1ac8d64227804633db6986d5c76cf0ab8666dbb1
Leila Abdi <leila.abdi@student.moringaschool.com>
2023-12-16 09:12:07 +0300

    completed assigment

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function receivesAFunction(callback) {
+    callback();
+}
+
+module.exports = {
+    receivesAFunction,
+
+};
+
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+    
+    };
+}
+
+module.exports = {
+    returnsANamedFunction,
+    
+};
+
+
+function returnsAnAnonymousFunction() {
+    return function() {
+    };
+}
+
+module.exports = {
+    returnsAnAnonymousFunction,
+    
+};
+
